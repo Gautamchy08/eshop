3bed02d312468ba444964fec46b8eb6b5fa92cb3
Gautam Chy <gautamchy8789@gmail.com>
2025-10-24 13:41:42 +0530

    completed monorepo setup

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,57 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import * as path from 'path';
+import cors from 'cors';
+import proxy from 'express-http-proxy'
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import swaggerUi from 'swagger-ui-express';
+import axios from 'axios';
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+
+app.use(cors(
+  {
+    origin : ['http://localhost:3000'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }
+));
+  
+app.use(morgan('dev'));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(cookieParser());
+app.set('trust proxy', 1);
+
+
+// Apply rate limiting to all requests
+
+ const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max : (req:any)=>(req.user?.id ? 1000 : 100), // 1000 requests per 15 minutes for logged in users, 100 for guests
+
+  message : {error : 'Too many requests, please try again later.'},
+  standardHeaders: true,
+  legacyHeaders: true,
+  keyGenerator: (req:any) => req.ip
+
+});
+
+app.use(limiter);
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to api-gateway!' });
+});
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
